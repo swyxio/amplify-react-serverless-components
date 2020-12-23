e71029440467597d36eef80a6fc10e242f10f825
swyx <wanshawn@amazon.com>
2020-12-24 07:35:54 +0800

    add first tasks copying over

---
+++ amplify/backend/function/rsc/src/reactApp/App.server.js
@@ -0,0 +1,10 @@
+
+// import {Suspense} from 'react';
+
+export default function App({selectedId, isEditing, searchText}) {
+  return (
+    <div className="main">
+      Hello from App.server.js
+    </div>
+  );
+}
